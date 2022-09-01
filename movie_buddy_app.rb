5ef8b6fa5c728daec0cadac3b622cc0a310932bd
Joseph McCann <joe.mccann@mailbox.org>
2022-09-01 14:55:18 -0400

    Remove results display logic from view.

--- movie_buddy_app.rb
+++ movie_buddy_app.rb
@@ -2,13 +2,32 @@ require 'sinatra'
 require './movie_finder'
 
 get '/' do
-  if params[:title]
-    @finder = MovieFinder.new(params[:title], params[:release_year])
-    @results = @finder.movies
-  end
+  @finder = MovieFinder.new(params[:title], params[:release_year]) if params[:title]
 
   erb :layout, layout: false do
     erb :index
   end
 end
-  
+
+helpers do
+  def results(finder)    
+    return unless finder
+
+    return api_limit_reached_message if finder.api_limit_reached
+
+    @movies = finder.movies
+    return no_results_message unless @movies
+    
+    erb :results, collection: @movies
+  end
+
+  def api_limit_reached_message
+    '<p class="error">The api_key for this app has reached
+    its daily limit of allotted requests.
+    Try again tomorrow.</p>'
+  end
+
+  def no_results_message
+    '<p class="error">No results found.</p>'
+  end
+end
